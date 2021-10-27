8f4fc04e47f6a66bec90d8e86ff524b30fcb7d5d
Sander Janssens <sander.janssens@outlook.be>
2021-10-27 14:44:38 +0200

    Initial commit

---
+++ src/test/java/com/switchfullywork/eurder/EurderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfullywork.eurder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

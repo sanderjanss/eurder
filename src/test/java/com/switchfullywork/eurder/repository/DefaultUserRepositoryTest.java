d5315fb5f2d46951fcb3b20dcaa6a2e13c5628a6
Sander Janssens <sander.janssens@outlook.be>
2021-11-02 17:06:20 +0100

    added mockito functionality to DefaultUserRepositoryTest

--- src/test/java/com/switchfullywork/eurder/repository/DefaultUserRepositoryTest.java
+++ src/test/java/com/switchfullywork/eurder/repository/DefaultUserRepositoryTest.java
@@ -7,54 +7,40 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 class DefaultUserRepositoryTest {
 
-    @Autowired
     private UserRepository userRepository;
     private User customer1;
     private User customer2;
 
     @BeforeEach
     public void before() {
-        userRepository = new DefaultUserRepository();
+        userRepository = mock(DefaultUserRepository.class);
         Address address1 = new Address("LegendStreet", "2000", "Antwerp", 1);
         Address address2 = new Address("SpookyStreet", "2000", "Antwerp", 1);
-        Address address3 = new Address("AdminStreet", "2000", "Antwerp", 1);
         customer1 = new User("Sander", "Janssens", "sanderzz@email.com", address1, "0488888888", Role.CUSTOMER);
         customer2 = new User("Bram", "Janssens", "Bramzz@email.com", address2, "0477777777", Role.CUSTOMER);
-        User admin1 = new User("Admin", "Janssens", "Admin@email.com", address3, "0411111111", Role.ADMIN);
-
+        userRepository.registerCustomer(customer1);
+        userRepository.registerCustomer(customer2);
     }
 
     @Test
     @DisplayName("Return all customers when no customerId is provided.")
     public void givenATestUserRepositoryWithValidCustomers_whenNoCustomerIdIsProvided_thenReturnAllCustomersFromDatabase() {
-        //when
-        userRepository.registerCustomer(customer1);
-        userRepository.registerCustomer(customer2);
 
-        //then
-        org.assertj.core.api.Assertions.assertThat(userRepository.getAllCustomers(null)).containsAll(List.of(customer2, customer1));
+        when(userRepository.getAllCustomers(null)).thenReturn(List.of(customer2, customer1));
     }
 
     @Test
     @DisplayName("Return the customer who's id is provided.")
     public void givenATestUserRepository_whenRegisteringValidCustomers_whenProvidedAValidCustomerId_thenReturnValidCustomer() {
-        //when
-        userRepository.registerCustomer(customer1);
-        userRepository.registerCustomer(customer2);
 
-        //then
-        Assertions.assertEquals(userRepository.getAllCustomers(customer2.getUserId()), List.of(customer2));
+        when(userRepository.getAllCustomers(customer2.getUserId())).thenReturn(List.of(customer2));
     }
 
 

35b00b6b3a4043ee52035f1103b0e6249e8087f1
Sander Janssens <sander.janssens@outlook.be>
2021-11-03 13:59:58 +0100

    Refactored ItemDTO, ItemGroupDTO to lombok classes

--- src/main/java/com/switchfullywork/eurder/domain/item/ItemDTO.java
+++ src/main/java/com/switchfullywork/eurder/domain/item/ItemDTO.java
@@ -1,8 +1,14 @@
 package com.switchfullywork.eurder.domain.item;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
+@Builder
+@AllArgsConstructor
 public class ItemDTO {
 
     private final UUID itemId;
@@ -11,81 +17,4 @@ public class ItemDTO {
     private final double price;
     private final int amountStock;
 
-    public ItemDTO(UUID itemId, String name, String description, double price, int amountStock) {
-        this.itemId = itemId;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.amountStock = amountStock;
-    }
-
-    public UUID getItemId() {
-        return itemId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public int getAmountStock() {
-        return amountStock;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{" +
-                "id=" + itemId +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", amountStock=" + amountStock +
-                '}';
-    }
-
-
-    public static class ItemDTOBuilder {
-
-        private UUID itemId;
-        private String name;
-        private String description;
-        private double price;
-        private int amountStock;
-
-        public ItemDTOBuilder setItemId(UUID itemId) {
-            this.itemId = itemId;
-            return this;
-        }
-
-        public ItemDTOBuilder setName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public ItemDTOBuilder setDescription(String description) {
-            this.description = description;
-            return this;
-        }
-
-        public ItemDTOBuilder setPrice(double price) {
-            this.price = price;
-            return this;
-        }
-
-        public ItemDTOBuilder setAmountStock(int amountStock) {
-            this.amountStock = amountStock;
-            return this;
-        }
-
-        public ItemDTO build() {
-            return new ItemDTO(this.itemId, this.name, this.description, this.price, this.amountStock);
-        }
-    }
 }
--- src/main/java/com/switchfullywork/eurder/domain/item/ItemGroupDTO.java
+++ src/main/java/com/switchfullywork/eurder/domain/item/ItemGroupDTO.java
@@ -1,70 +1,19 @@
 package com.switchfullywork.eurder.domain.item;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Data
+@Builder
+@AllArgsConstructor
 public class ItemGroupDTO {
     private final UUID itemGroupId;
     private final UUID itemId;
     private final int amount;
     private final LocalDate shippingDate;
 
-    public ItemGroupDTO(UUID itemGroupId, UUID itemId, int amount, LocalDate shippingDate) {
-        this.itemGroupId = itemGroupId;
-        this.itemId = itemId;
-        this.amount = amount;
-        this.shippingDate = calulateShippingDate(shippingDate);
-    }
-
-    public UUID getItemGroupId() {
-        return itemGroupId;
-    }
-
-    public UUID getItemId() {
-        return itemId;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public LocalDate getShippingDate() {
-        return shippingDate;
-    }
-
-    private LocalDate calulateShippingDate(LocalDate shippingDate) {
-        return shippingDate;
-    }
-
-    public static class ItemGroupDTOBuilder {
-
-        private UUID itemGroupId;
-        private UUID itemid;
-        private int amount;
-        private LocalDate shippingDate;
-
-        public ItemGroupDTOBuilder setItemGroupId(UUID itemGroupId) {
-            this.itemGroupId = itemGroupId;
-            return this;
-        }
-
-        public ItemGroupDTOBuilder setItemid(UUID itemid) {
-            this.itemid = itemid;
-            return this;
-        }
-
-        public ItemGroupDTOBuilder setAmount(int amount) {
-            this.amount = amount;
-            return this;
-        }
-
-        public ItemGroupDTOBuilder setShippingDate(LocalDate shippingDate) {
-            this.shippingDate = shippingDate;
-            return this;
-        }
-
-        public ItemGroupDTO build() {
-            return new ItemGroupDTO(this.itemGroupId, this.itemid, this.amount, this.shippingDate);
-        }
-    }
 }

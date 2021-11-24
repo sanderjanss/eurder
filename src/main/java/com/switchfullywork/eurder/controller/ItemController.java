a651fe0bb078d6035020d4096889cb6d0016f4b8
Sander Janssens <sander.janssens@outlook.be>
2021-11-24 11:30:40 +0100

    added findAll for items functionality

--- src/main/java/com/switchfullywork/eurder/controller/ItemController.java
+++ src/main/java/com/switchfullywork/eurder/controller/ItemController.java
@@ -1,12 +1,15 @@
 package com.switchfullywork.eurder.controller;
 
 import com.switchfullywork.eurder.domain.itemdto.CreateItemRequest;
+import com.switchfullywork.eurder.domain.itemdto.ItemResponse;
 import com.switchfullywork.eurder.service.ItemService;
 import com.switchfullywork.eurder.switchsecure.SecurityGuard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/items")
@@ -24,16 +27,26 @@ public class ItemController {
     @SecurityGuard(SecurityGuard.ApiUserRole.ADMIN)
     public void registerItem(@RequestBody CreateItemRequest createItemRequest) {
         logger.info("Registering item.");
-        itemService.registerItem(createItemRequest);
-        logger.info("Item registered.");
+        ItemResponse itemResponse = itemService.registerItem(createItemRequest);
+        logger.info("Item registered with id: " + itemResponse.getItemId());
     }
 
     @PutMapping(consumes = "application/json", path = "/{itemId}")
     @ResponseStatus(HttpStatus.OK)
     @SecurityGuard(SecurityGuard.ApiUserRole.ADMIN)
     public void updateItem(@RequestBody CreateItemRequest createItemRequest, @PathVariable(value = "itemId") int itemId) {
-        logger.info("Updating item.");
+        logger.info("Updating item with id: " + itemId);
         itemService.updateItem(createItemRequest, itemId);
-        logger.info("Updated item.");
+        logger.info("Updated item with id: " + itemId);
+    }
+
+    @GetMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @SecurityGuard(SecurityGuard.ApiUserRole.CUSTOMER)
+    public List<ItemResponse> getAllItems(){
+        logger.info("Asking up all items.");
+        List<ItemResponse> itemResponses = itemService.getAllItems();
+        logger.info(String.format("Retrieved %s item.", itemResponses.size()));
+        return itemResponses;
     }
 }
--- src/main/java/com/switchfullywork/eurder/repository/ItemRepository.java
+++ src/main/java/com/switchfullywork/eurder/repository/ItemRepository.java
@@ -1,11 +1,16 @@
 package com.switchfullywork.eurder.repository;
 
 import com.switchfullywork.eurder.domain.entity.item.Item;
+import com.switchfullywork.eurder.domain.itemdto.ItemResponse;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ItemRepository extends CrudRepository<Item, Integer> {
 
     Item findItemByName(String name);
 
     Item findItemByItemId(int itemId);
+
+    List<Item> findAll();
 }
